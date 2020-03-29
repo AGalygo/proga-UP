48fd5655685cc807d02bc4eb62560270d15c9cb1
Alena <Nat_030178@mail.ru>
2020-03-29 19:59:19 +0300

    Add 9-10 task

--- task6/main.js
+++ task6/main.js
@@ -5,7 +5,9 @@ var posts = [ //массив постов
             'тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-01-17T23:00:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '2',
@@ -13,7 +15,9 @@ var posts = [ //массив постов
             'тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-01-20T23:11:00'),
         author: 'Галыго Алена',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '3',
@@ -21,14 +25,18 @@ var posts = [ //массив постов
             'спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-02-17T21:00:00'),
         author: 'Галыго Алена',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '4',
         description: 'Это четвертый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-02-19T19:21:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '5',
@@ -36,7 +44,9 @@ var posts = [ //массив постов
             'спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-02-21T23:00:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '6',
@@ -44,105 +54,135 @@ var posts = [ //массив постов
             'спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-02-22T11:15:00'),
         author: 'Иванов Иван',
-        photoLink: ''
+        photoLink: '',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '7',
         description: 'Это седьмой пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-02-25T12:05:00'),
         author: 'Галыго Алена',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '8',
         description: 'Это восьмой пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-01T14:47:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '9',
         description: 'Это девятый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-05T22:19:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '10',
         description: 'Это десятый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-05T23:28:00'),
         author: 'Литвинко Вика',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '11',
         description: 'Это одиннадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-12T08:14:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '12',
         description: 'Это двенадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-12T10:45:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '13',
         description: 'Это тринадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-13T23:00:00'),
         author: 'Литвинко Вика',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: []
     },
     {
         id: '14',
         description: 'Это четырнадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-15T17:25:00'),
         author: 'Литвинко Вика',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '15',
         description: 'Это пятнадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-17T20:00:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#health'],
+        likes: ['Иванов Иван']
     },
     {
         id: '16',
         description: 'Это шеснадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-18T11:11:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '17',
         description: 'Это семнадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-19T23:00:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: [],
+        likes: ['Alena_G']
     },
     {
         id: '18',
         description: 'Это восемнадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-19T23:12:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '19',
         description: 'Это девятнадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-21T23:12:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#love'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
     {
         id: '20',
         description: 'Это двадцатый пост! Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
         createdAt: new Date('2020-03-25T23:13:00'),
         author: 'Иванов Иван',
-        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
+        photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+        hashTags: ['#ill', '#health'],
+        likes: ['Alena_G', 'Иванов Иван']
     },
 ];
 
@@ -167,7 +207,16 @@ var posts = [ //массив постов
           return false;
       if (typeof post.photoLink !== "string") //необязательное поле
           return false;
-      return true;
+      if(post.hashTags.length == 0) {
+          return true;
+      }
+      else
+         for (let i = 0; i < post.hashTags.length; i++) {
+              if (!(post.hashTags[i].substring(0, 1) === '#')) {
+                  return false;
+              }
+          }
+         return true;
   }
 //validatePost(posts[5]);
 function getPosts(skip, top, filterConfig) {
@@ -191,7 +240,7 @@ function getPosts(skip, top, filterConfig) {
                 }
             )).slice(skip, skip + top);
         }
-        debugger;
+
         if (filterConfig.dateFrom) {
             return posts.filter(function (post) {
                 return post.createdAt.getTime() >= filterConfig.dateFrom.getTime();
@@ -209,6 +258,19 @@ function getPosts(skip, top, filterConfig) {
                     return dateA - dateB
                 }
             )).slice(skip, skip + top);
+        }
+        if (filterConfig.hashTagSearch) {
+                return posts.filter(function (post) {
+                    for (var hashTag of post.hashTags) {
+                            if (hashTag === filterConfig.hashTagSearch) {
+                                return true;
+                            }
+                        }
+                    }).sort((function (a, b) {
+                        var dateA = new Date(a.createdAt), dateB = new Date(b.createdAt)
+                        return dateA - dateB
+                    }
+                )).slice(skip, skip + top);
         }
     }
    }
@@ -246,6 +308,9 @@ function editPost(id, post) {
     }
     if (post.photoLink) {
         posts[num].photoLink = post.photoLink;
+    }
+    if (post.hashTags) {
+        posts[num].hashTags = post.hashTags;
     }
     return true;
    }
